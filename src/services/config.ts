c49756ee9c41932869826e3414743f258de6393c
Belousov Vadim <thevadiratti@yandex.ru>
2024-07-19 15:49:59 +0500

    Добавляет config для сервисов

---
+++ src/services/config.ts
@@ -0,0 +1,14 @@
+export default {
+  API_ORIGIN: 'https://sandbox.creos.me',
+  API_ENDPOINT: '/api',
+  API_VERSION: '/v1',
+
+  endpoints: {
+    // Эндпоинты названы в единственном числе, хотя отдают коллекции сущностей.
+    // Они должны заканчиваться на -s.
+    comments: '/comment/', // '/comments' и т.д.
+    projects: '/project',
+    designers: '/designer',
+    issues: '/issue',
+  },
+} as const;
