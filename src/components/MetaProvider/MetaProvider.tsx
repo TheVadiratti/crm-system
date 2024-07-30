9734049faae9cb78873efabfb2d8bffdc4a70f76
Belousov Vadim <thevadiratti@yandex.ru>
2024-07-30 16:36:50 +0500

    Добавляет сохранение метаданных после перезагрузки страницы (redux-persist)

--- src/components/MetaProvider/MetaProvider.tsx
+++ src/components/MetaProvider/MetaProvider.tsx
@@ -1,11 +1,9 @@
 import { type ReactNode, useMemo, useEffect } from 'react';
-import { createTheme, useMediaQuery } from '@mui/material';
+import { createTheme } from '@mui/material';
 import * as locales from '@mui/material/locale';
 import MuiThemeProvider from '@mui/material/styles/ThemeProvider';
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from '../../store';
-import { changeTheme } from '../../store/meta';
-import { Themes } from '../../utils/constants';
+import { useSelector } from '../../store';
 import getLangFromLocaleName from '../../utils/getLangFromLocaleName';
 
 interface Props {
@@ -13,18 +11,10 @@ interface Props {
 }
 
 export default function MetaProvider({ children }: Props) {
-  const dispatch = useDispatch();
   const { theme: mode, locale: localeName } = useSelector(
     (state) => state.meta
   );
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const { i18n } = useTranslation();
-
-  useEffect(() => {
-    if (prefersDarkMode) {
-      dispatch(changeTheme(Themes.dark));
-    }
-  }, [dispatch, prefersDarkMode]);
 
   useEffect(() => {
     i18n.changeLanguage(getLangFromLocaleName(localeName));
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import MetaProvider from './components/MetaProvider';
 import './index.css';
 import RouterProvider from './router';
 import { store } from './store';
+import { persistor } from './store/store';
 import './utils/i18n/i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <MetaProvider>
-        <CssBaseline />
-        <RouterProvider />
-      </MetaProvider>
+      <PersistGate persistor={persistor}>
+        <MetaProvider>
+          <CssBaseline />
+          <RouterProvider />
+        </MetaProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
