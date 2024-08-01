1364edbe01e3fa2cae863ad7cffe70839417a41f
Belousov Vadim <thevadiratti@yandex.ru>
2024-08-01 14:23:45 +0500

    Рефакторит функцию маппинга данных DoneIssuesChartSection

--- src/components/PageSection/DoneIssuesChartSection/mappers/fromDtoToChartData/fromDtoToChartData.ts
+++ src/components/PageSection/DoneIssuesChartSection/mappers/fromDtoToChartData/fromDtoToChartData.ts
@@ -3,76 +3,47 @@ import type { IssueDto } from '../../../../../services/types.dto';
 import getWorkWeek from '../../../../../utils/getWorkWeek';
 import type { DoneIssueChartData } from './types';
 
-function getSumIssuesData(data: DoneIssueChartData[]): DoneIssueChartData[] {
-  const result: DoneIssueChartData[] = [];
-
-  const doneIssuesGroupedData = lodash.groupBy(data, 'workWeekNumber');
-
-  // Перебор ключей с помощью Object.keys используется по указанию ESLint.
-  const doneIssuesGroupedDataObjectKeys = Object.keys(doneIssuesGroupedData);
-
-  doneIssuesGroupedDataObjectKeys.forEach((key) => {
-    let Revenue = 0;
-    let Expenses = 0;
-    let Profit = 0;
-
-    doneIssuesGroupedData[key].forEach((issue) => {
-      Revenue += issue.Revenue;
-      Expenses += issue.Expenses;
-      Profit += issue.Revenue - issue.Expenses;
-    });
-
-    result.push({
-      workWeekNumber: Number(key),
-      Revenue,
-      Expenses,
-      Profit,
-    });
-  });
-
-  return result;
-}
-
-// Функция будет работать правильно при условии передачи в неё данных за 1 календарный (рабочий) год.
 export default function mapFromDtoToChartData(
   issuesList: IssueDto[],
   nLatestWeek: number
 ): DoneIssueChartData[] | [] {
-  const doneIssuesList = issuesList.filter((issue) => issue.status === 'Done');
-  const latestWeekByList = lodash.maxBy(doneIssuesList, (o) =>
-    getWorkWeek(new Date(o.date_created))
-  );
+  let result: DoneIssueChartData[] = [];
 
-  if (latestWeekByList) {
-    const latestDoneIssuesList = doneIssuesList.filter(
-      (issue) =>
-        getWorkWeek(new Date(issue.date_created)) >
-        getWorkWeek(new Date(latestWeekByList.date_created)) - nLatestWeek
-    );
+  issuesList.forEach((issue) => {
+    if (issue.status === 'Done') {
+      const Revenue = issue.received_from_client;
+      const Expenses =
+        issue.send_to_designer +
+        issue.send_to_account_manager +
+        issue.send_to_project_manager;
+      const Profit = Revenue - Expenses;
+      const workWeekNumber = getWorkWeek(new Date(issue.date_created));
 
-    // Подсчитывает необходимые показатели для каждой issue.
-    const calculatedNotGroupedDoneIssues: DoneIssueChartData[] =
-      latestDoneIssuesList.map((issue) => {
-        const Revenue = issue.received_from_client;
-        const Expenses =
-          issue.send_to_designer +
-          issue.send_to_account_manager +
-          issue.send_to_project_manager;
+      let hasInResult = false;
 
-        return {
-          workWeekNumber: getWorkWeek(new Date(issue.date_created)),
-          Revenue,
-          Expenses,
-          Profit: Revenue - Expenses,
-        };
+      result = result.map((item) => {
+        if (item.workWeekNumber === workWeekNumber) {
+          hasInResult = true;
+          return {
+            workWeekNumber,
+            Revenue: item.Revenue + Revenue,
+            Expenses: item.Expenses + Expenses,
+            Profit: item.Profit + Profit,
+          };
+        }
+        return item;
       });
 
-    // Суммирует показатели по каждой неделе и сортирует по возрастанию.
-    return lodash.sortBy(
-      getSumIssuesData(calculatedNotGroupedDoneIssues),
-      'workWeekNumber'
-    );
-  }
+      if (!hasInResult) {
+        result.push({
+          workWeekNumber,
+          Revenue,
+          Expenses,
+          Profit,
+        });
+      }
+    }
+  });
 
-  return [];
+  return lodash.sortBy(result, ['workWeekNumber']).slice(-nLatestWeek);
 }
